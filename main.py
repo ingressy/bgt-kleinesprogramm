13596731efcc5406d7874b5fe4526a86bbfcbaf2
ingressy <j.czinzoll@ingressy.de>
2024-11-08 09:53:13 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,30 @@
+import tkinter as tk
+from tkinter import ttk
+
+def calc_u():
+    root_u = tk.Tk()
+    root_u.title("Just another URI Calc | U")
+    root_u.bind('<Escape>', lambda e: root_u.destroy())
+    root_u.protocol("WM_DELETE_WINDOW", root_u.iconify)
+    root_u.geometry("360x240")
+
+    label1 = ttk.Label(root_u, text='Widerstandswert | R', font=("Arial", 12))
+    label1.pack(ipadx=10, ipady=10)
+
+    R = tk.StringVar()
+    textbox = ttk.Entry(root_u, textvariable=R)
+    textbox.pack(fill='x', expand=True)
+    R = label1.get()
+
+    label2 = ttk.Label(root_u, text='Ampere Wert | I', font=("Arial", 12))
+    label2.pack(ipadx=10, ipady=10)
+    I = tk.StringVar()
+    textbox = ttk.Entry(root_u, textvariable=I)
+    textbox.pack(fill='x', expand=True)
+    I = label2.get()
+
+    output = R * I
+    print(output, "V")
 
 def uri(auswahl, schaltung, U, R, I):
     if auswahl == "U":
@@ -72,7 +99,43 @@ def werte(auswahl, schaltung):
     uri(auswahl, schaltung, U, R, I)
 
 def main():
-    print("Welchen Wert möchtest du ausrechnen? | U R I")
+    root = tk.Tk()
+    root.title("Just another URI Calc")
+    root.bind('<Escape>', lambda e: root.destroy())
+    root.protocol("WM_DELETE_WINDOW", root.iconify)
+    root.geometry("720x480")
+
+    label1 = ttk.Label(root, text='Moin!', font=("Arial",16))
+    label2 = ttk.Label(root, text='Welchen Wert möchtest du ausrechnen?', font=("Arial", 12))
+    label1.pack(ipadx=10, ipady=10)
+    label2.pack(ipadx=5, ipady=5)
+
+    button_u = ttk.Button(root, text="U", command=calc_u )
+    button_r = ttk.Button(root, text="R", )
+    button_i = ttk.Button(root, text="I",)
+
+    button_u.pack(
+        ipadx=5,
+        ipady=5,
+        expand=True,
+        side = tk.LEFT
+    )
+    button_r.pack(
+        ipadx=5,
+        ipady=5,
+        expand=False,
+        side=tk.LEFT
+    )
+    button_i.pack(
+        ipadx=5,
+        ipady=5,
+        expand=True,
+        side=tk.LEFT
+    )
+
+    root.mainloop()
+
+    print(" | U R I")
     auswahl = input().upper()
 
     print("Parallel/Reihen -schaltung | P R")
